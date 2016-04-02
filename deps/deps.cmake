bd6c423f996d0e24dc683b02bee75efa62fe4ca3
Matt Blair <blair1618@gmail.com>
2016-04-02 02:20:45 -0400

    Add GLM submodule and CMake config

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -1,5 +1,5 @@
 set(STOCK_DEPS_DIR deps)
-set(STOCK_INCLUDES "")
+set(STOCK_INCLUDE_DIRS "")
 set(STOCK_LIBRARIES "")
 
 ## GLFW ##
@@ -13,7 +13,7 @@ set(GLFW_INSTALL OFF CACHE BOOL "Generate installation target")
 add_subdirectory(${STOCK_DEPS_DIR}/glfw)
 target_compile_options(glfw PRIVATE -O3)
 target_compile_definitions(glfw PRIVATE NDEBUG)
-set(STOCK_INCLUDES "${STOCK_INCLUDES} ${STOCK_DEPS_DIR}/glfw/include")
+list(APPEND STOCK_INCLUDE_DIRS ${STOCK_DEPS_DIR}/glfw/include)
 list(APPEND STOCK_LIBRARIES glfw)
 list(APPEND STOCK_LIBRARIES ${GLFW_LIBRARIES})
 
@@ -21,3 +21,7 @@ list(APPEND STOCK_LIBRARIES ${GLFW_LIBRARIES})
 
 find_package(OpenGL REQUIRED)
 list(APPEND STOCK_LIBRARIES ${OPENGL_LIBRARIES})
+
+## GLM ##
+
+list(APPEND STOCK_INCLUDE_DIRS ${STOCK_DEPS_DIR}/glm)
