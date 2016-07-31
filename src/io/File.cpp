f058b58e3f053982a5130c6283f1d759faf4a8a3
Matt Blair <blair1618@gmail.com>
2016-07-31 16:53:54 -0400

    Add a very simple File class

---
+++ src/io/File.cpp
@@ -0,0 +1,81 @@
+//
+// Created by Matt Blair on 7/31/16.
+//
+
+#include "io/File.hpp"
+
+namespace stock {
+
+File::File(const std::string& path) : m_path(path) {}
+
+File::~File() {
+    close();
+}
+
+bool File::open() {
+
+    if (!m_isOpen) {
+        m_handle = fopen(m_path.c_str(), "rb");
+        return m_handle != nullptr;
+    }
+
+    return false;
+
+}
+
+bool File::close() {
+
+    if (m_isOpen) {
+        return fclose(m_handle) == 0;
+    }
+
+    return false;
+
+}
+
+bool File::exists() {
+
+    open();
+    return m_handle != nullptr;
+
+}
+
+size_t File::getSize() {
+
+    if (!exists()) {
+        return 0;
+    }
+
+    fseek(m_handle, 0, SEEK_END);
+    size_t size = ftell(m_handle);
+    fseek(m_handle, 0, SEEK_SET);
+
+    return size;
+
+}
+
+size_t File::read(uint8_t* buffer, size_t start, size_t count) {
+
+    if (!exists()) {
+        return 0;
+    }
+
+    fseek(m_handle, start, SEEK_SET);
+    size_t read = fread(buffer, 1, count, m_handle);
+    fseek(m_handle, 0, SEEK_SET);
+
+    return read;
+
+}
+
+std::vector<uint8_t> File::readAll() {
+
+    auto size = getSize();
+    std::vector<uint8_t> result(size, 0);
+    read(result.data(), 0, size);
+
+    return result;
+
+}
+
+}
---
+++ src/io/File.hpp
@@ -0,0 +1,55 @@
+//
+// Created by Matt Blair on 7/31/16.
+//
+
+#pragma once
+
+#include <cstdio>
+#include <string>
+#include <vector>
+
+namespace stock {
+
+class File {
+
+public:
+
+    // Create a File representing the given path.
+    File(const std::string& path);
+
+    // Destroy the File object and close any open path.
+    ~File();
+
+    // Open the path for this File; returns true if opening was successful, returns false if opening was unsuccessful or
+    // if the File was already opened.
+    bool open();
+
+    // Close the path for this File; returns true if closing was successful, returns false if closing was unsuccessful
+    // or if the File was not opened.
+    bool close();
+
+    // Get whether this File object represents an existing path in the filesystem; if the File is not open this will
+    // attempt to open it.
+    bool exists();
+
+    // Get the size of this File in bytes; if the File is not open this will attempt to open it; returns 0 if the File
+    // does not exist.
+    size_t getSize();
+
+    // Starting at 'start' bytes into the File, read 'count' bytes of the File and copy them into 'buffer'; if the File
+    // is not open this will attempt to open it; returns the number of bytes successfully read.
+    size_t read(uint8_t* buffer, size_t start, size_t count);
+
+    // Read the entire contents of the file into a vector of bytes; if the File is not open this will attempt to open
+    // it; if the File does not exist the returned vector is empty.
+    std::vector<uint8_t> readAll();
+
+protected:
+
+    std::string m_path;
+    FILE* m_handle = nullptr;
+    bool m_isOpen = false;
+
+};
+
+} // namespace stock
