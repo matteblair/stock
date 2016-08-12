288c0d9ce462c5ff9a329e662f3bca81c3137f53
Matt Blair <blair1618@gmail.com>
2016-08-11 21:55:44 -0400

    Add a beautiful tetrahedron as a rendering test

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 #include "gl/Mesh.hpp"
 #include "gl/RenderState.hpp"
 #include "gl/ShaderProgram.hpp"
+#include "view/Camera.hpp"
 
 using namespace stock;
 
 const std::string vs_src = R"SHADER_END(
-attribute vec2 a_position;
+attribute vec3 a_position;
 attribute vec4 a_color;
 varying vec4 v_color;
+uniform mat4 u_mvp;
 void main() {
     v_color = a_color;
-    gl_Position = vec4(a_position, 1., 1.);
+    gl_Position = u_mvp * vec4(a_position, 1.);
 }
 )SHADER_END";
 
@@ -24,7 +26,7 @@ void main() {
 )SHADER_END";
 
 struct Vertex {
-    float x, y;
+    float x, y, z;
     unsigned int color;
 };
 
@@ -49,30 +51,46 @@ int main(void) {
 
     ShaderProgram shader;
     shader.setSourceStrings(fs_src, vs_src);
+    UniformLocation mvpMatrixLocation("u_mvp");
 
     Mesh<Vertex> mesh;
     mesh.setVertexLayout(VertexLayout(
         {
-            VertexAttribute("a_position", 2, GL_FLOAT, false),
-            VertexAttribute("a_color", 4, GL_UNSIGNED_BYTE, true),
+            VertexAttribute("a_position", 3, GL_FLOAT, GL_FALSE),
+            VertexAttribute("a_color", 4, GL_UNSIGNED_BYTE, GL_TRUE),
         }
     ));
     mesh.vertices = {
-        { 1.f, 0.f, 0xffff0000 },
-        { .5f, 1.f, 0xff00ff00 },
-        { 0.f, 0.f, 0xff0000ff },
+        {  1.f,  1.f,  1.f, 0xff000000 },
+        { -1.f,  1.f, -1.f, 0xffff0000 },
+        {  1.f, -1.f, -1.f, 0xff00ff00 },
+        { -1.f, -1.f,  1.f, 0xff0000ff },
     };
+    mesh.indices = { 0, 1, 2, 0, 3, 1, 0, 2, 3, 1, 3, 2 };
 
     RenderState rs;
     rs.configure();
     rs.clearColor(0.f, 0.f, 0.f, 1.f);
+    rs.depthTest(GL_TRUE);
 
-    Log::setLevel(stock::Log::Level::VERBOSE);
+    Camera camera;
+    camera.setWidth(1024.f);
+    camera.setHeight(768.f);
+    camera.setPosition(0.f, -3.f, 0.f);
+
+    Log::setLevel(Log::Level::VERBOSE);
 
     // Loop until the user closes the window.
     while (!glfwWindowShouldClose(window)) {
         // Render here.
         CHECK_GL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+
+        static const glm::vec3 point = { 0.f, 0.f, 0.f }, axis = { 0.f, 0.f, 1.f };
+        camera.orbit(point, axis, 0.016f);
+        camera.lookAt(point);
+        camera.update();
+
+        shader.setUniformMatrix4f(rs, mvpMatrixLocation, camera.viewProjectionMatrix());
 
         mesh.draw(rs, shader);
 
