fff557e96a304ae552962edcaf11a4648bf7c638
Matt Blair <blair1618@gmail.com>
2019-03-06 02:47:28 -0500

    Re-implement VecDeque using raw array for storage
    TODO:
    - abstraction for construction/destruction
    - avoid allocation for empty container, if possible
    - copy/move assignment/constructors

--- src/containers/VecDeque.hpp
+++ src/containers/VecDeque.hpp
@@ -6,7 +6,7 @@
 
 #include <cassert>
 #include <cstddef>
-#include <vector>
+#include <memory>
 
 namespace stock {
 
@@ -18,12 +18,14 @@ namespace stock {
  *
  * @tparam T The contained type.
  */
-template<typename T>
+template<typename T, class Allocator = std::allocator<T>>
 class VecDeque {
 
 public:
 
   explicit VecDeque(size_t capacity = 0);
+
+  ~VecDeque();
 
   bool empty() const;
 
@@ -57,150 +59,167 @@ public:
 
 protected:
 
-  using Container = std::vector<T>;
-
-  Container m_container;
+  T* m_buffer = nullptr;
+  size_t m_capacity = 0;
   size_t m_begin = 0;
   size_t m_end = 0;
 
   void grow_if_needed();
 };
 
-template<typename T>
-VecDeque<T>::VecDeque(size_t capacity) {
-  m_container.resize(capacity + 1);
+template<typename T, class Allocator>
+VecDeque<T, Allocator>::VecDeque(size_t capacity) {
+  m_capacity = capacity + 1;
+  m_buffer = Allocator().allocate(m_capacity);
 }
 
-template<typename T>
-bool VecDeque<T>::empty() const {
+template<typename T, class Allocator>
+VecDeque<T, Allocator>::~VecDeque() {
+  Allocator().deallocate(m_buffer, m_capacity);
+}
+
+template<typename T, class Allocator>
+bool VecDeque<T, Allocator>::empty() const {
   return m_begin == m_end;
 }
 
-template<typename T>
-bool VecDeque<T>::full() const {
-  return m_container.size() - size() == 1;
+template<typename T, class Allocator>
+bool VecDeque<T, Allocator>::full() const {
+  return m_capacity - size() == 1;
 }
 
-template<typename T>
-size_t VecDeque<T>::size() const {
+template<typename T, class Allocator>
+size_t VecDeque<T, Allocator>::size() const {
   if (m_end >= m_begin) {
     return m_end - m_begin;
   }
-  return (m_container.size() - m_begin) + m_end;
+  return (m_capacity - m_begin) + m_end;
 }
 
-template<typename T>
-size_t VecDeque<T>::capacity() const {
-  return m_container.size() - 1;
+template<typename T, class Allocator>
+size_t VecDeque<T, Allocator>::capacity() const {
+  return m_capacity - 1;
 }
 
-template<typename T>
-void VecDeque<T>::clear() {
-  m_container.clear();
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::clear() {
+  auto read = m_begin;
+  while (read != m_end) {
+    if (read == m_capacity) {
+      read = 0;
+    } else {
+      m_buffer[read].~T();
+      read++;
+    }
+  }
   m_begin = 0;
   m_end = m_begin;
 }
 
-template<typename T>
-T& VecDeque<T>::front() {
+template<typename T, class Allocator>
+T& VecDeque<T, Allocator>::front() {
   assert(m_begin != m_end);
-  return m_container[m_begin];
+  return m_buffer[m_begin];
 }
 
-template<typename T>
-const T& VecDeque<T>::front() const {
+template<typename T, class Allocator>
+const T& VecDeque<T, Allocator>::front() const {
   assert(m_begin != m_end);
-  return m_container[m_begin];
+  return m_buffer[m_begin];
 }
 
-template<typename T>
-T& VecDeque<T>::back() {
+template<typename T, class Allocator>
+T& VecDeque<T, Allocator>::back() {
   assert(m_begin != m_end);
   if (m_end == 0) {
-    return m_container.back();
+    return m_buffer[m_capacity - 1];
   }
-  return m_container[m_end - 1];
+  return m_buffer[m_end - 1];
 }
 
-template<typename T>
-const T& VecDeque<T>::back() const {
+template<typename T, class Allocator>
+const T& VecDeque<T, Allocator>::back() const {
   assert(m_begin != m_end);
   if (m_end == 0) {
-    return m_container.back();
+    return m_buffer[m_capacity - 1];
   }
-  return m_container[m_end - 1];
+  return m_buffer[m_end - 1];
 }
 
-template<typename T>
-void VecDeque<T>::pop_front() {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::pop_front() {
   assert(m_begin != m_end);
-  if (++m_begin == m_container.size()) {
+  if (++m_begin == m_capacity) {
     m_begin = 0;
   }
 }
 
-template<typename T>
-void VecDeque<T>::pop_back() {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::pop_back() {
   assert(m_begin != m_end);
   if (m_end == 0) {
-    m_end = m_container.size();
+    m_end = m_capacity;
   }
   m_end--;
 }
 
-template<typename T>
-void VecDeque<T>::push_front(const T& value) {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::push_front(const T& value) {
   grow_if_needed();
   if (m_begin == 0) {
-    m_begin = m_container.size();
+    m_begin = m_capacity;
   }
   m_begin--;
-  m_container[m_begin] = value;
+  new(&m_buffer[m_begin]) T(value);
 }
 
-template<typename T>
-void VecDeque<T>::push_front(T&& value) {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::push_front(T&& value) {
   grow_if_needed();
   if (m_begin == 0) {
-    m_begin = m_container.size();
+    m_begin = m_capacity;
   }
   m_begin--;
-  m_container[m_begin] = std::move(value);
+  new(&m_buffer[m_begin]) T(std::move(value));
 }
 
-template<typename T>
-void VecDeque<T>::push_back(const T& value) {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::push_back(const T& value) {
   grow_if_needed();
-  m_container[m_end] = value;
+  new(&m_buffer[m_end]) T(value);
   m_end++;
-  if (m_end == m_container.size()) {
+  if (m_end == m_capacity) {
     m_end = 0;
   }
 }
 
-template<typename T>
-void VecDeque<T>::push_back(T&& value) {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::push_back(T&& value) {
   grow_if_needed();
-  m_container[m_end] = std::move(value);
+  new(&m_buffer[m_end]) T(std::move(value));
   m_end++;
-  if (m_end == m_container.size()) {
+  if (m_end == m_capacity) {
     m_end = 0;
   }
 }
 
-template<typename T>
-void VecDeque<T>::grow_if_needed() {
+template<typename T, class Allocator>
+void VecDeque<T, Allocator>::grow_if_needed() {
   if (full()) {
     // Create an expanded container to replace our full one.
-    Container expanded(m_container.size() * 2);
+    size_t expandedCapacity = m_capacity * 2;
+    T* expanded = Allocator().allocate(expandedCapacity);
     // Move existing elements into new container.
     size_t write = 0;
     auto read = m_begin;
     while (read != m_end) {
-      if (read == m_container.size()) {
+      if (read == m_capacity) {
         read = 0;
       } else {
-        expanded[write] = std::move(m_container[read]);
+        // Construct the entry in the new buffer using placement-new.
+        new(&expanded[write]) T(std::move(m_buffer[read]));
+        // Destruct the entry in the old buffer.
+        m_buffer[read].~T();
         read++;
         write++;
       }
@@ -209,7 +228,9 @@ void VecDeque<T>::grow_if_needed() {
     m_begin = 0;
     m_end = write;
     // Swap containers.
-    m_container.swap(expanded);
+    Allocator().deallocate(m_buffer, m_capacity);
+    m_buffer = expanded;
+    m_capacity = expandedCapacity;
   }
 }
 
